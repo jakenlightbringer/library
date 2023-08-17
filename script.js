8d38ee285ad41f881ef992427ca434f5616a7903
jakenlightbringer <mememe49bf4@gmail.com>
2023-08-16 19:14:34 -0700

    fillout table check display logic

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Book(author, title, pages, read){
     this.author = author;
     this.title = title;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
     
 }
 
@@ -20,66 +20,28 @@ const addBookToLibrary = () => {
     let title = $titleInput.value;
     let author = $authorInput.value;
     let pages = $pagesInput.value;
-    //let read = need get checked box value
+    let read = $readInput.value;
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
 const addButton = document.getElementById("addBookButton");
 const formContainer = document.getElementById('form-container');
+const bookContainer = document.getElementById('book-container');
 
-addButton.addEventListener('click', e=>{
-    openForm();
-});
-formContainer.addEventListener('click', e => {
-    if (e.target === formContainer) {
-        closeForm();
+myLibrary.forEach(book =>{
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages`;
+    if (book.read) {
+        bookDiv.textContent += " (Read)";
+    } else {
+        bookDiv.textContent += " (Not Read)";
     }
+    libraryContainer.appendChild(bookDiv);
+
+    
 });
 
-function openForm(){
-
-    const form = document.createElement('form');
-    form.classList.add('form-popup');
-    const titleLabel = document.createElement('label');
-    titleLabel.textContent = 'Book Title';
-    const titleInput = document.createElement('input');
-    titleInput.type = 'text';
-    titleInput.name = 'title';
-    titleLabel.appendChild(titleInput);
-    const authorLabel = document.createElement('label');
-    authorLabel.textContent = "Author:";
-    const authorInput = document.createElement('input');
-    authorInput.type = 'text';
-    authorInput.name = 'title';
-    authorLabel.appendChild(authorInput);
-    const pagesLabel = document.createElement('label');
-    pagesLabel.textContent = 'Pages';
-    const pagesInput = document.createElement('input');
-    pagesInput.type = 'text';
-    pagesInput.name = 'pages';
-    pagesLabel.appendChild(pagesInput);
-    
 
 
-
-    const submitButton = document.createElement('button')
-    submitButton.type = 'submit';
-    submitButton.textContent = 'Add Book';
-
-    form.appendChild(titleLabel);
-    form.appendChild(authorLabel);
-    form.appendChild(pagesLabel);
-    form.append(document.createElement('br'));
-    form.appendChild(submitButton);
-
-
-    formContainer.appendChild(form);
-    formContainer.style.display = "flex"
-}
-
-function closeForm(){
-    //add close button inside of formContainer, display set to none, add to flex on click for openForm --> this needs to be within Clsoe form function
-    formContainer.innerHTML = "";
-    formContainer.style.display = "none";
-}
